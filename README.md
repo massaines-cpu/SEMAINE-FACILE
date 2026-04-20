efffcf310baf39a7b860d1379f4417253a5d69a5
inesmassa <inesurvivor@gmail.com>
2026-04-20 16:57:06 +0200

    fin crea table, debut colonnes

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 ETL = Extract Transform Load
+
+Extract :
+
+- Extraire les donnnées
+
+Tranformation :
+
+- nettoyage
+- normalisation des données
+- fusion des données
+- -champ de dérivation
+
+Load :
+- les données sont déplacées vers leur destination locale
---
+++ pipeline_ETL.py
@@ -0,0 +1,60 @@
+import pandas as pd
+import sqlite3
+
+# Faire un pipeline ETL pour des données numériques.
+#
+# 1. en entrée du pipeline il y a l’url d’un fichier csv.
+# 2. le système analyse et extrait les données.
+# 3. le système les enregistre dans une base SQLite
+
+fichier_csv = 'Data1.csv'
+
+data = pd.read_csv(fichier_csv)
+colonnes = data.columns
+print(colonnes)
+
+connexion = sqlite3.connect("egg_base.db")
+
+connexion.execute('''
+    CREATE TABLE IF NOT EXISTS source (
+        id    INTEGER PRIMARY KEY,
+        nom   TEXT,
+        url   TEXT)
+''')
+
+connexion.execute('''
+    CREATE TABLE IF NOT EXISTS colonne (
+        id        INTEGER PRIMARY KEY,
+        label     TEXT,
+        type      TEXT,
+        source_id INTEGER)
+''')
+
+connexion.execute('''
+    CREATE TABLE IF NOT EXISTS data (
+        id        INTEGER PRIMARY KEY,
+        value     TEXT,
+        column_id INTEGER,
+        row_id    INTEGER,
+        ordre     INTEGER)
+''')
+connexion.commit()
+connexion.execute('INSERT INTO source (nom) VALUES (?)', ('Data1.csv',))
+connexion.commit()
+
+print('ok')
+
+connexion.execute(
+    'INSERT INTO source (nom, url) VALUES (?, ?)',
+    ('Data1.csv', 'Data1.csv'))
+connexion.commit()
+
+print('ok2')
+
+for colonne in colonnes:
+    pass
+# ouvrir le fichier
+# ajoute la source nom du fichier etc
+# recup la liste des col avec leur types
+# ajoute chaque col dans la bdd
+# pareil avec les data
