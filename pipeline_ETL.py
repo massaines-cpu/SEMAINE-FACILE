75d6f97088317fee6e9bd5c21edd52290e70530b
inesmassa <inesurvivor@gmail.com>
2026-04-21 10:14:09 +0200

    colonne

--- pipeline_ETL.py
+++ pipeline_ETL.py
@@ -1,12 +1,6 @@
 import pandas as pd
 import sqlite3
 
-# Faire un pipeline ETL pour des données numériques.
-#
-# 1. en entrée du pipeline il y a l’url d’un fichier csv.
-# 2. le système analyse et extrait les données.
-# 3. le système les enregistre dans une base SQLite
-
 fichier_csv = 'Data1.csv'
 
 data = pd.read_csv(fichier_csv)
@@ -15,23 +9,20 @@ print(colonnes)
 
 connexion = sqlite3.connect("egg_base.db")
 
-connexion.execute('''
-    CREATE TABLE IF NOT EXISTS source (
+connexion.execute('''CREATE TABLE IF NOT EXISTS source (
         id    INTEGER PRIMARY KEY,
         nom   TEXT,
         url   TEXT)
 ''')
 
-connexion.execute('''
-    CREATE TABLE IF NOT EXISTS colonne (
+connexion.execute('''CREATE TABLE IF NOT EXISTS colonne (
         id        INTEGER PRIMARY KEY,
         label     TEXT,
         type      TEXT,
         source_id INTEGER)
 ''')
 
-connexion.execute('''
-    CREATE TABLE IF NOT EXISTS data (
+connexion.execute(''' CREATE TABLE IF NOT EXISTS data (
         id        INTEGER PRIMARY KEY,
         value     TEXT,
         column_id INTEGER,
@@ -42,14 +33,19 @@ connexion.commit()
 
 print('ok')
 
-connexion.execute(
-    'INSERT INTO source (nom, url) VALUES (?, ?)',('Data1.csv', 'Data1.csv'))
+curseur = connexion.execute('INSERT INTO source (nom, url) VALUES (?, ?)',('Data1.csv', 'Data1.csv'))
 connexion.commit()
+
+id_source = curseur.lastrowid
 
 print('ok2')
 
 for colonne in colonnes:
-    pass
+    type_colonne = str(data[colonne].dtype)
+    connexion.execute('INSERT INTO colonne (label, type, source_id) VALUES (?, ?, ?)', (colonne, type_colonne, id_source))
+    connexion.commit()
+
+print('ok3')
 # ouvrir le fichier
 # ajoute la source nom du fichier etc
 # recup la liste des col avec leur types
